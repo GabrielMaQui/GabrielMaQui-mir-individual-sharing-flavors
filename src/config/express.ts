7034d1846e366f586dc2df30e7bf7de844a08630
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-11-28 12:10:42 -0500

    chore: :tada: initial commit

---
+++ src/config/express.ts
@@ -0,0 +1,13 @@
+import express, { type Application } from 'express';
+
+function configExpress(app: Application): void {
+  app.use(express.json());
+
+  //cors
+
+  //morgan
+
+  //urlencoded
+}
+
+export default configExpress;
---
+++ src/router.ts
@@ -0,0 +1,7 @@
+import type { Application } from 'express';
+
+function routes(app: Application): void {
+  /*app.use('/api/user', userRouter)*/
+}
+
+export default routes;
