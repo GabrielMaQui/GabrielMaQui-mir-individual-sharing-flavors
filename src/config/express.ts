7fe4384c77704566c8ede68048f85efa9b193c22
WLupacaTicona <135378021+PunoBootcamper@users.noreply.github.com>
2024-12-03 18:16:33 -0500

    feat: add cors and morgan (#3)

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,12 +1,15 @@
+import cors from 'cors';
 import express, { type Application } from 'express';
+import morgan from 'morgan';
 
 function configExpress(app: Application): void {
   app.use(express.json());
 
   //cors
+  app.use(cors());
 
   //morgan
-
+  app.use(morgan('dev'));
   //urlencoded
 }
 
