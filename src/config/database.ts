d73ae2ba8a221ca923d950a5e9465cbf36acd3a6
William Lupaca <wlupacat@gmail.com>
2024-12-08 18:21:58 -0500

    feat: add favorites

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
 
-//const dbURL = process.env.DB_URL || "mongodb+srv://Gabriel_Steven:GABRIEL29pros@cluster1.fbb65zu.mongodb.net/sharing-flavors?retryWrites=true&w=majority&appName=Cluster1";
 const dbURL =
   process.env.DB_URL ||
-  'mongodb+srv://wlupacat:abDMuMnuKa2Ynks2@cluster0.i2e1x.mongodb.net/sharing-flavors?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://Gabriel_Steven:GABRIEL29pros@cluster1.fbb65zu.mongodb.net/sharing-flavors?retryWrites=true&w=majority&appName=Cluster1';
 console.log(process.env.DB_URL);
 async function connectDB() {
   await mongoose
